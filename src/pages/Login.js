8ff3c590b04cb1c709e41ecb8263d6c938645af4
Manuel Diaz <diasfer17@gmail.com>
2020-02-09 23:55:34 -0500

    Login: se realizo el desarrollo de la interfaz del login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,32 @@
-import React from 'react';
-
+import React from "react";
+import "./Login.css";
 const Login = () => {
-    return ( 
-        <h1>Login en Desarrollo</h1>
-     );
-}
- 
+  return (
+    <div className="Login__page">
+      <div className="Login_page_container">
+        <div className="Login__card">
+          <h2>Inicia Sesion</h2>
+          <form className="Login_container_form">
+            <input
+              className="Form__input"
+              type="text"
+              placeholder="Usuario"
+            />
+            <input
+              className="Form__input"
+              type="password"
+              placeholder="Contraseña"
+            />
+            <button className="Form__button">Iniciar sesion</button>
+              <a href="#!" className="Form__remember_me">Olvide mi contraseña</a>
+          </form>
+          <p className="Form__register">
+            ¿No tienes cuenta? <a href="#!" className="Form__remember_me">Registrate</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Login;
